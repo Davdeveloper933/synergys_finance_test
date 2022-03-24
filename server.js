5804873b1304deda309b65ef89fbe3e9e8d5f60d
david istomin <daveistomin@gmail.com>
2022-03-24 06:45:07 +0400

    Сверстал до блока How it works.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const cors = require("cors");
 app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+// app.use(serveStatic(path.join(__dirname, "dist")));
 app.use(cors());
 const port = process.env.PORT || 80;
 app.listen(port);
