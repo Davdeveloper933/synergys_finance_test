ac5c4f1de409862b84f84a8831802de61f4eb9c8
david istomin <daveistomin@gmail.com>
2022-03-24 16:16:23 +0400

    make it better

--- server.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const express = require("express");
-const serveStatic = require("serve-static");
-const cors = require("cors");
-app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
-app.use(cors());
-const port = process.env.PORT || 3000;
-app.listen(port);
