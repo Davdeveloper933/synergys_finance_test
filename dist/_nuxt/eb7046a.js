52686a1e887e359002ed84a7ab11a04a8805e7ab
david istomin <daveistomin@gmail.com>
2022-03-29 19:37:55 +0400

    in progress

---
+++ dist/_nuxt/eb7046a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{314:function(e,t,n){"use strict";n.r(t);var c={name:"SiteFooter"},o=n(45),component=Object(o.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"528662c1",null);t.default=component.exports}}]);
